f7755024f46f895d501947e98b790e644ab4b1c8
Isaac Amortegui <isaacamortegc@outlook.com>
2023-04-12 19:34:23 -0500

    Swapping function added

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System; 
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Array Numbers Swapper");
+        Console.WriteLine("Enter the array values, separated by commas:");
+        string input = Console.ReadLine();
+        string[] values = input.Split(',');
+        int[] array = new int[values.Length];
+
+        for (int i = 0; i< values.Length; i++)
+        {
+            array[i] = int.Parse(values[i]);
+        }
+
+        int swap;
+
+        for (int i = 0; i < array.Length / 2; i++)
+        {
+            swap = array[i];
+            array[i] = array[array.Length - 1 - i];
+            array[array.Length - 1 - i] = swap;
+        }
+    }
+}
